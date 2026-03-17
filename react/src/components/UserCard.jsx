dc68749e2cd684ceb6d21b86e21a8a8f4215a13e
BhavyaLuhana <shayamsundarluhana609@gmail.com>
2026-03-17 22:55:56 +0530

    adding some components

---
+++ react/src/components/UserCard.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const UserCard = () => {
+  return (
+    <div>
+      <h1>Radha</h1>
+      <img id='user-image' src='bhavya'></img>
+      <p className='description'>My name is Radha, i am queen of the world</p>
+    </div>
+  )
+}
+
+export default UserCard;
